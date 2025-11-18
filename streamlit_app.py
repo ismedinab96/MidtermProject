91066c6a13adf6eb325abd86fef63ee8ef253deb
ismedinab.96 <128383796+ismedinab96@users.noreply.github.com>
2025-11-18 03:21:49 +0000

    last time

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ import numpy as np
 st.title("🛍️ ML Zoomcamp Cohort 2025 ")
 st.write("Upload your details to predict product rating!")
 st.write("Elaborated by Iver Samuel Medina Balboa - ZoomCap ML Bootcamp - IA student from Computer Science at UMSA")
+st.write("Tania Mujica My love for support ❤️")
 
 
 with open("model/model.pkl", "rb") as f:
@@ -16,7 +17,7 @@ with st.form("prediction_form"):
     gender = st.selectbox("Gender", ["Male", "Female"])
     age = st.slider("Age", 18, 100, 30)
     category = st.selectbox("Category", ["Clothing", "Electronics", "Books", "Home", "Beauty"])
-    submit = st.form_submit_button("Predecir")
+    submit = st.form_submit_button("Prediction")
 
     if submit:
         input_data = {"gender": gender, "age": age, "category": category}
